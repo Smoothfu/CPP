634cfce1140f396ccc6b23ae12c5c84820b34157
Fred <2864326836@qq.com>
2023-01-01 21:17:37 +0800

    jsoncpp serialize vector into plain json file

--- main.cpp
+++ main.cpp
@@ -36,16 +36,16 @@ void deserialize_json_string_to_vector_demo(const string &filename)
     ul.deserialize_json_string_to_vector(std::ref(filename));
 }
 
-void invoke_func_argv_demo(int x,int y)
+void invoke_func_argv_demo(int x, int y)
 {
     util ul;
-    ul.invoke_func_argv(x,y);
+    ul.invoke_func_argv(x, y);
 }
 
-void multi_thread_demo(int x,int y,int z,string str)
+void multi_thread_demo(int x, int y, int z, string str)
 {
     util ul;
-    ul.multi_thread(x,y,z,str);
+    ul.multi_thread(x, y, z, str);
 }
 
 void print_random_demo(int len)
@@ -54,10 +54,10 @@ void print_random_demo(int len)
     ul.print_random(len);
 }
 
-void t_array_demo(uint32_t min,uint32_t max,int len)
+void t_array_demo(uint32_t min, uint32_t max, int len)
 {
     util ul;
-    ul.t_array_demo(min,max,len);
+    ul.t_array_demo(min, max, len);
 }
 
 void quick_sort_demo(int len)
@@ -72,25 +72,25 @@ void bubble_sort_asc_demo(int len)
     ul.bubble_sort_asc_uint64(len);
 }
 
-int sum(int x,int y)
+int sum(int x, int y)
 {
-    return x+y;
+    return x + y;
 }
 
-int prod(int x,int y)
+int prod(int x, int y)
 {
-    return x*y;
+    return x * y;
 }
 
-int pass_func(int x,int y,int(*func)(int,int))
+int pass_func(int x, int y, int (*func)(int, int))
 {
-    return func(x,y);
+    return func(x, y);
 }
 
-void pass_func_demo(int x,int y)
+void pass_func_demo(int x, int y)
 {
-    cout<<"The sum of "<<x<<" and "<<y<<" is "<<pass_func(x,y,&sum)<<endl;
-    cout<<"The prod of "<<x<<" and "<<y<<" is "<<pass_func(x,y,&prod)<<endl;
+    cout << "The sum of " << x << " and " << y << " is " << pass_func(x, y, &sum) << endl;
+    cout << "The prod of " << x << " and " << y << " is " << pass_func(x, y, &prod) << endl;
 }
 
 void retrieve_data_from_mysql()
@@ -99,56 +99,8 @@ void retrieve_data_from_mysql()
     ul.mysql_retrieve_data();
 }
 
-//g++ -g -std=c++2a -I. *.cpp ./model/*.cpp -o h1 -luuid -lpthread -ljsoncpp -lmysqlcppconn
-int main(int args,char **argv)
-{ 
-    // t_array_demo(0,UINT32_MAX,atoi(argv[1]));
-    // quick_sort_demo(atoi(argv[1]));
-    // bubble_sort_asc_demo(atoi(argv[1]));
-    // pass_func_demo(atoi(argv[1]),atoi(argv[2]));
-    retrieve_data_from_mysql();
+// g++ -g -std=c++2a -I. *.cpp ./model/*.cpp -o h1 -luuid -lpthread -ljsoncpp -lmysqlcppconn
+int main(int args, char **argv)
+{
+    serialize_book_vector_demo(atoi(argv[1]));
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
